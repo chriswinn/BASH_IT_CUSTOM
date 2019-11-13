d5a96c7959de8a833db716ebd33989def1139dda
Chris Winn <chriswinn@gmail.com>
2019-11-13 16:10:43 -0500

    refactored tmux config notes out of bash_profile. Also refactored and cleaned up the _.bash file

--- CUSTOM/_.bash
+++ CUSTOM/_.bash
@@ -4,6 +4,7 @@
 # Bash Turn Colors On
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
+export HOSTNAME=chriswinn
 # Turn off Bash Session Recording
 #SHELL_SESSION_HISTORY=0
 
@@ -16,7 +17,7 @@ if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
 # ================ Golang Config ====================
 
-GOPATH=~/.go/bin:$GOPATH
+#GOPATH=~/.go/bin:$GOPATH
 
 # ================ Python 3 Config ====================
 
@@ -33,6 +34,11 @@ export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 # workon                           # List available virtual environments
 # workon name_of_environment       # Activate the specified Python virtual environment
 # rmvirtualenv name_of_environment # Remove the specified environment
+
+# ================ Ionic Config ====================
+
+# export IONIC_PERSISTOR_PASSWORD for - Personal "Gmail" Acct.
+export IONIC_PERSISTOR_PASSWORD=mysecret
 
 # ================ Django Notes ====================
 
@@ -56,7 +62,7 @@ export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 #     aws --version
 #   To upgrade to the latest AWS CLI Tool
 #     pip3 install awscli --upgrade --user
-complete -C '/Users/c17n/Library/Python/3.7/bin/aws_completer' aws         
+#complete -C '/Users/c17n/Library/Python/3.7/bin/aws_completer' aws         
 # AWS Environment Variables
 #   export AWS_ACCOUNT_ID=""     ## e.g. 123456789012
 #   export AWS_DEFAULT_REGION="" ## e.g. us-east-1
@@ -84,32 +90,43 @@ fi
 
 # ================ NCURSES Config ====================
 
-# ncurses is keg-only, which means it was not symlinked into /usr/local,
-# because macOS already provides this software and installing another version in
-# parallel can cause all kinds of trouble.
+#  ncurses is keg-only, which means it was not symlinked into /usr/local,
+#  because macOS already provides this software and installing another version in
+#  parallel can cause all kinds of trouble.
 #
-#If you need to have ncurses first in your PATH run:
-#  echo 'export PATH="/usr/local/opt/ncurses/bin:$PATH"' >> ~/.bash_profile
-#For compilers to find ncurses you may need to set:
-#  export LDFLAGS="-L/usr/local/opt/ncurses/lib"
-#  export CPPFLAGS="-I/usr/local/opt/ncurses/include"
-#For pkg-config to find ncurses you may need to set:
-#  export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig"
-
-# ================ BASH Set Default Editor ====================
-
-# Just to set default editor while inside tmux
-export EDITOR='vim'
+#  If you need to have ncurses first in your PATH run:
+#    echo 'export PATH="/usr/local/opt/ncurses/bin:$PATH"' >> ~/.bash_profile
+#  For compilers to find ncurses you may need to set:
+#    export LDFLAGS="-L/usr/local/opt/ncurses/lib"
+#    export CPPFLAGS="-I/usr/local/opt/ncurses/include"
+#  For pkg-config to find ncurses you may need to set:
+#    export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig"
 
 # ================ Tmux Config ====================
 
-#Example configuration has been installed to:
-#  /usr/local/opt/tmux/share/tmux
+#  Example configuration has been installed to:
+#    /usr/local/opt/tmux/share/tmux
+#
+#  Set Xterm/screen/Tmux title with only a short hostname.
+#    Uncomment this (or set SHORT_HOSTNAME to something else),
+#    Will otherwise fall back on $HOSTNAME.
+#export SHORT_HOSTNAME=$(hostname -s)
+#
+#  Set Xterm/screen/Tmux title with only a short username.
+#    Uncomment this (or set SHORT_USER to something else),
+#    Will otherwise fall back on $USER.
+#export SHORT_USER=${USER:0:8}
+#
+#  Set Xterm/screen/Tmux title with shortened command and directory.
+#    Uncomment this to set.
+#export SHORT_TERM_LINE=true
+#
+# Just to set default editor while inside tmux
+export EDITOR='vim'
+
 alias tmux-sane="tmux -u new-session \; split-window -h -p 70 \; split-window -v -p 65 \; select-pane -t 0 \; send-keys 'vim $PWD/.' C-m \;"
-#alias tmux-dev="tmux -u new-session \; split-window -h -p 70 \; send-keys 'brew list && pip list' C-m \; split-window -v -p 65 \; send-keys 'vim $PWD/.' C-m \; select-pane -t 1 \; send-keys 'vim $PWD/.' C-m \;"
+#  alias tmux-dev="tmux -u new-session \; split-window -h -p 70 \; send-keys 'brew list && pip list' C-m \; split-window -v -p 65 \; send-keys 'vim $PWD/.' C-m \; select-pane -t 1 \; send-keys 'vim $PWD/.' C-m \;"
 alias tmux-dev="tmux -u new-session \; split-window -h -p 70 \; send-keys 'pip list' C-m \; split-window -v -p 65 \; send-keys 'brew list' C-m \; select-pane -t 1 \; send-keys 'vim $PWD/.' C-m \;"
-#Bash completion has been installed to:
-#  /usr/local/etc/bash_completion.d
 
 # ================ BASH Aliases ====================
 
